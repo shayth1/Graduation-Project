1ebb5e79c19141811d7057c75118d510857dc445
shayth1 <shaythwork@gmail.com>
2020-12-19 00:52:49 +0200

    01:00am chnging update where cond.

--- api/updateUser.php
+++ api/updateUser.php
@@ -1,20 +1,22 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <?php 
 require "../config.php";
-
-    $ID = $_POST['ide'];
+ 
+    
     $First_Name = $_POST['First_Name'];
+    // $ID = $_POST['ide'];
     $Last_Name = $_POST['Last_Name'];
     $Phone = $_POST['Phone'];
     $Email = $_POST['Email'];
 
     $query = "UPDATE medicalteamlogin SET First_Name = '$First_Name', Last_Name = '$Last_Name', Phone = '$Phone', Email = '$Email'
-    WHERE ID = '$ID' ";
+    WHERE Email = '$Email' AND Phone = '$Phone' ";
     $update = mysqli_query($conn, $query);
     if ($update) {
         header("Location: ../ModifyUsers.php");
+        
     }else{
         echo '<div class="alert alert-danger" role="alert">
        Oops Someting went wrong';
@@ -22,3 +24,5 @@ require "../config.php";
 
 
 ?>
+<script src="../vendor/jquery/jquery.slim.min.js"></script>
+<script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script> 
